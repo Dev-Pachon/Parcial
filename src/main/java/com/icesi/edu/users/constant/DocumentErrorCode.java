296053bc729c55b3ecc247e3698e688971dc34ff
Diego H <d13g01hl@gmail.com>
2022-09-13 15:06:10 -0500

    Passed controller working test

--- src/main/java/com/icesi/edu/users/constant/DocumentErrorCode.java
+++ src/main/java/com/icesi/edu/users/constant/DocumentErrorCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @Getter
 public enum DocumentErrorCode {
 
-    CODE_01("fix this");
+    CODE_01("fix this"),
+    CODE_02("Only letters and spaces are allowed in name");
 
     private String message;
 }
--- src/main/java/com/icesi/edu/users/controller/DocumentController.java
+++ src/main/java/com/icesi/edu/users/controller/DocumentController.java
@@ -1,10 +1,14 @@
 package com.icesi.edu.users.controller;
 
 import com.icesi.edu.users.api.DocumentAPI;
+import com.icesi.edu.users.constant.DocumentErrorCode;
 import com.icesi.edu.users.dto.DocumentDTO;
+import com.icesi.edu.users.error.exception.DocumentError;
+import com.icesi.edu.users.error.exception.DocumentException;
 import com.icesi.edu.users.mapper.DocumentMapper;
 import com.icesi.edu.users.service.DocumentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,9 +23,21 @@ public class DocumentController implements DocumentAPI {
 
     private final DocumentService documentService;
 
+    private final String DOCUMENT_NAME_REGEX = "^[A-Za-z\\s]*$";
+
     @Override
     public DocumentDTO createDocument(DocumentDTO documentDTO) {
-        return documentMapper.fromDocument(documentService.createDocument(documentMapper.fromDTO(documentDTO)));
+        if(isNotNull(documentDTO) && isNotNull(documentDTO.getName()) && checkDocumentName(documentDTO.getName()))
+            return documentMapper.fromDocument(documentService.createDocument(documentMapper.fromDTO(documentDTO)));
+        throw new DocumentException(HttpStatus.BAD_REQUEST, new DocumentError(DocumentErrorCode.CODE_02, DocumentErrorCode.CODE_02.getMessage()));
+    }
+
+    private boolean isNotNull(Object toCheck) {
+        return toCheck != null;
+    }
+
+    private boolean checkDocumentName(String name) {
+        return name.matches(DOCUMENT_NAME_REGEX);
     }
 
     @Override
